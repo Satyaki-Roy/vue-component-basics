775ac5fb48acbbacb6ef1191f197f34a5921a1d5
Satyaki Roy <satyakiroy001@gmail.com>
2020-11-04 18:04:59 +0530

    init commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <h2>Friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from "@/components/FriendContact";
+export default {
+  name: "App",
+  components: {FriendContact},
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "09876 543 221",
+          email: "manuel@localhost.com",
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,36 @@
+<template>
+  <li>
+    <h2>{{friend.name}}</h2>
+    <button @click="showDetails">{{detailsAreVisible ? "Hide" : "Show"}} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{friend.phone}}</li>
+      <li><strong>Email:</strong>{{friend.email}}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "FriendContact",
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "01234 5678 991",
+        email: "manuel@localhost.com",
+      },
+    }
+  },
+  methods: {
+    showDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
